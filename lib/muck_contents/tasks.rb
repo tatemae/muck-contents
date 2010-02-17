24ee2a0319a60dc9830a847c5bf4ace8cd0b440f
Justin Ball <justinball@gmail.com>
2010-02-17 15:59:41 -0700

    updated translations.  refactored tasks

--- lib/muck_contents/tasks.rb
+++ lib/muck_contents/tasks.rb
@@ -12,9 +12,9 @@ module MuckContents
     def define
       
       namespace :muck do
-        namespace :contents do
+        namespace :sync do
           desc "Sync required files from muck contents."
-          task :sync do
+          task :contents do
             path = File.join(File.dirname(__FILE__), *%w[.. ..])
             system "rsync -ruv #{path}/db ."
             system "rsync -ruv #{path}/public ."
