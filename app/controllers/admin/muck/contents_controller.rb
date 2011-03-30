12f8b5232d6ad42b2172a1ff0cd320289b5a3100
Justin Ball <justinball@gmail.com>
2011-03-30 00:29:27 -0600

    Fixed autosearch

--- app/controllers/admin/muck/contents_controller.rb
+++ app/controllers/admin/muck/contents_controller.rb
@@ -1,9 +1,32 @@
 class Admin::Muck::ContentsController < Admin::Muck::BaseController
   
   def index
-    @contents = Content.by_title.by_newest
     respond_to do |format|
-      format.html { render :template => 'admin/contents/index' }
+      format.html do
+        @contents = Content.by_title.by_newest.paginate(:page => @page, :per_page => @per_page)
+        render :template => 'admin/contents/index' 
+      end
+      format.json do
+        @contents = Content.by_title.by_newest.where("title like ?", params[:term] + '%').paginate(:page => @page, :per_page => @per_page)
+        render :json => @contents
+      end
+    end
+  end
+  
+  def destroy
+    @content = Content.find(params[:id])
+    @content.destroy
+    respond_to do |format|
+      format.html do
+        flash[:notice] = t('muck.contents.content_removed')
+        redirect_to admin_contents_path
+      end
+      format.js do
+        render(:update) do |page|
+          page << "jQuery('##{@content.dom_id}').fadeOut();"
+        end
+      end
+      format.json { render :json => { :success => true, :message => t("muck.contents.content_removed"), :content_id => @content.id } }
     end
   end
   
--- test/spec/controllers/admin/contents_controller_spec.rb
+++ test/spec/controllers/admin/contents_controller_spec.rb
@@ -19,12 +19,53 @@ describe Admin::Muck::ContentsController do
       end
       
       describe "GET index" do
-        before do
-          get :index
+        describe "html" do
+          before do
+            get :index
+          end
+          it { should respond_with :success }
+          it { should render_template :index }
         end
-        it { should respond_with :success }
-        it { should render_template :index }
+        describe "json" do
+          before do
+            @content = Factory(:content, :title => 'my test doc')
+            get :index, :format => 'json', :term => 'my'
+          end
+          it { should respond_with :success }
+          it "should include the searched for content" do
+            @response.body.should include(@content.title)
+          end
+        end
       end    
+      
+      describe "DELETE destroy" do
+        before do
+          @user = Factory(:user)
+          @content = Factory(:content, :contentable => @user)
+        end
+        describe "html" do
+          before do
+            delete :destroy, :id => @content.id
+          end
+          it { should respond_with :redirect }
+          it { should set_the_flash.to(I18n.t('muck.contents.content_removed')) }
+        end
+        describe "js" do
+          before do
+            delete :destroy, :id => @content.id, :format => 'js'
+          end
+          it { should respond_with :success }
+        end
+        describe "json" do
+          before do
+            delete :destroy, :id => @content.id, :format => 'json'
+          end
+          it { should respond_with :success }
+          it "should indicate success in the json" do
+            @response.body.should include(I18n.t("muck.contents.content_removed"))
+          end
+        end
+      end
       
     end
   end
